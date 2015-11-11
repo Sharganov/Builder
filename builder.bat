adfa8341e239a5bbd44c17464e75d430542cead7
Sharganov Artem <sharganov.artem@yandex.ru>
2015-11-11 22:56:13 +0300

    Bag fix

--- builder.bat
+++ builder.bat
@@ -14,7 +14,7 @@ if ERRORLEVEL 1 goto :cloneError
 echo Cloning is successful
 
 echo Build...
-%MSBuildPath%\MSBuild.exe %repositoryPath%\geometry.sln /p:Configuration=Release;VisualStudioVersion=12.0 >msbuild.log
+%MSBuildPath%\MSBuild.exe %repositoryPath%\%solutionName% /p:Configuration=Release;VisualStudioVersion=12.0 >msbuild.log
 if ERRORLEVEL 1 goto :buildError
 echo Building is successful
 
@@ -22,9 +22,11 @@ echo Checking build consistency...
 for /F "tokens=*" %%i in (files.txt) do if not exist %repositoryPath%\bin\Release\%%i set missingFile=%%i& goto :checkError
 
 echo Running tests...
-%NUnitPath%\nunit-console.exe %repositoryPath%\Tests\bin\Release\Tests.dll > tests.txt
+%NUnitPath%\nunit-console.exe %repositoryPath%\Test\bin\Release\Tests.dll > tests.txt
 if ERRORLEVEL 1 goto :testError
 echo Tests pased
+
+goto :sending
 
 :cloneError
 echo Cloning Failed
